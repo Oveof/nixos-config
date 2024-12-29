9aa69162e2fcbb76bb973e91220dc8582afbdc5c
oveof <ovedy@oftedal.com>
2024-12-29 21:30:41 +0100

    feat: added foot config

--- home/base/gui/terminal/foot/default.nix
+++ home/base/gui/terminal/foot/default.nix
@@ -3,4 +3,9 @@
   programs.foot = {
     enable = true;
   };
+  home.file.".config/foot" = {
+    source = ./config;
+    recursive = true;   # link recursively
+    executable = true;  # make all files executable
+  };
 }
