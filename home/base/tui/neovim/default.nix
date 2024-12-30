a360452588c5846a838755d0d4c2fe71bdcb481d
oveof <ovedy@oftedal.com>
2024-12-30 10:39:39 +0100

    fix: neovim

--- home/base/tui/neovim/default.nix
+++ home/base/tui/neovim/default.nix
@@ -3,9 +3,13 @@
   programs.neovim = {
     enable = true;
   };
-  home.file.".config/nvim" = {
-    source = config.lib.file.mkOutOfStoreSymlink ./nvim;
-    recursive = true;   # link recursively
-    executable = true;  # make all files executable
+  # home.file.".config/nvim" = {
+  #   source = config.lib.file.mkOutOfStoreSymlink ./nvim;
+  #   recursive = true;   # link recursively
+  #   executable = true;  # make all files executable
+  # };
+  xdg.configFile."nvim" = {
+    recursive = true;
+    source = ./nvim;
   };
 }
