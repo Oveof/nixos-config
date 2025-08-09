231b1050eb4cbfc27b2d4d5363f40122962f0ec2
Ove Oftedal <ovedy@oftedal.com>
2025-08-09 11:36:19 +0200

    feat: added userchrome.css to home manager

--- home/base/gui/firefox.nix
+++ home/base/gui/firefox.nix
@@ -1,5 +1,11 @@
 {
   programs.firefox = {
     enable = true;
+    profiles.default.userChrome = ''
+      /* hides the native tabs */
+        #TabsToolbar {
+          visibility: collapse;
+        }
+    '';
   };
 }
