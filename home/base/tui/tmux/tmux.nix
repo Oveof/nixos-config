7974e393ab2a7ccd6fe4067feaea70d61373e9e1
Ove Oftedal <ovedy@oftedal.com>
2026-01-26 07:38:09 +0100

    feat(tmux): added task warrior

--- home/base/tui/tmux/tmux.nix
+++ home/base/tui/tmux/tmux.nix
@@ -1,4 +1,22 @@
-{ pkgs, ... }:
+{
+  config,
+  pkgs,
+  # fetchFromGitHub,
+  ...
+}:
+let
+  tmux-grimoire = pkgs.tmuxPlugins.mkTmuxPlugin {
+    pluginName = "tmux-grimoire";
+    version = "1.0.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "navahas";
+      repo = "tmux-grimoire";
+      rev = "94b3f0087289ec5cbeb9cc225ab665661d55123c";
+      sha256 = "sha256-oLtOO0vclwwVLYnWCY0nG+QGAdYik6L04eR606IHnW0=";
+    };
+    rtpFilePath = "grimoire.tmux";
+  };
+in
 {
   programs.tmux = {
     enable = true;
@@ -11,6 +29,10 @@
       pkgs.tmuxPlugins.resurrect
       pkgs.tmuxPlugins.continuum
       pkgs.tmuxPlugins.yank
+      tmux-grimoire
+      pkgs.tmuxPlugins.tmux-floax
+      # needs python3 for some reason so idk
+      # pkgs.tmuxPlugins.tmux-which-key
     ];
     extraConfig = ''
       set -g base-index 1
@@ -27,6 +49,13 @@
       set-option -g prefix C-s
       set-window-option -g mode-keys vi
       bind C-o display-popup -E "tms"
+
+      bind-key -T prefix t run-shell "custom_shpell ephemeral taskwarrior '$HOME/.config/grimoire/shpells/popup-scratch.sh'"
+      set -g @shpell-taskwarrior-color '#e3716e'
+      set -g @shpell-taskwarrior-position 'center'
+      set -g @shpell-taskwarrior-width '80%'
+      set -g @shpell-taskwarrior-height '90%'
     '';
+
   };
 }
