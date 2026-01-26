9ac3fec13031d6740ce12de400bd1a0b87e7025b
Ove Oftedal <ovedy@oftedal.com>
2026-01-26 07:39:38 +0100

    feat: added more nix cache mirror

--- modules/nixos/base/nix.nix
+++ modules/nixos/base/nix.nix
@@ -1,7 +1,8 @@
 {
   lib,
   ...
-}: {
+}:
+{
   # to install chrome, you need to enable unfree packages
   nixpkgs.config.allowUnfree = lib.mkForce true;
 
@@ -15,6 +16,16 @@
   # Manual optimise storage: nix-store --optimise
   # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
   nix.settings.auto-optimise-store = true;
+  nix.settings = {
+    substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 
   nix.channel.enable = false; # remove nix-channel related tools & configs, we use flakes instead.
 }
